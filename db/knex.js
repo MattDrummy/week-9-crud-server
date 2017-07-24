5bf453401cc3914dc824514e48bdfdba13dca0d6
Matthew Drumheller <MattDrummy@gmail.com>
2017-07-24 15:51:42 -0600

    moved knexfile.js

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development'
 const knex = require('knex');
-const dbconfig = require('./knexfile')[environment];
+const dbconfig = require('../knexfile')[environment];
 
 module.exports = knex(dbconfig);
